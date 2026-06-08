ca6ab92a7d931e225d80aa78dd2ca75edbb5deda
Mario Kernich <mario.kernich@swan.de>
2026-06-08 19:15:06 +0200

    Initial Commit

---
+++ gym-tracker Watch App/gym_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gym_trackerApp.swift
+//  gym-tracker Watch App
+//
+//  Created by Mario on 08.06.26.
+//
+
+import SwiftUI
+
+@main
+struct gym_tracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
